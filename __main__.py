2cd3a1887f60fe2d28cb70e4ffe80c158deddc6e
Chigrinov <162994221+NikolayChigrinov@users.noreply.github.com>
2024-12-30 18:51:10 +0500

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("TinyTroupe запускается...")
+    # Здесь можно указать логику запуска, например:
+    from tinytroupe import main_function
+    main_function()
